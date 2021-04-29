c05549b3592653667501af5e87d33d88d670fefc
206819 <81298446+206819@users.noreply.github.com>
2021-04-29 14:57:25 +0530

    Update act1.h

--- inc/act1.h
+++ inc/act1.h
@@ -1,13 +1,4 @@
-/**
- * @file setup.h
- * @author vishal_274699
- * @brief header file for activity 1
- * @version 0.1
- * @date 2021-04-23
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 #ifndef ACT1_H_INCLUDED
 #define ACT1_H_INCLUDED
 
