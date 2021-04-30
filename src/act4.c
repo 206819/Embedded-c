dbc7510dfabcd55bc8ccc1bcb3918c4c6671c272
206819 <81298446+206819@users.noreply.github.com>
2021-04-30 21:27:07 +0530

    Update act4.c

--- src/act4.c
+++ src/act4.c
@@ -1,1 +1,37 @@
+/**
+ * @file act4.c
+ * @author 206819 (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<avr/io.h>
+#include<util/delay.h>
 
+void USARTInit(uint16_t ubrr_value)
+{
+
+    UBRR0L = ubrr_value;
+    UBRR0H = (ubrr_value>>8)&(0x00ff);
+    UCSR0C = (1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
+
+    UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
+
+}
+char USARTREADChar()
+{
+  while(!(UCSR0A&(1<<RXC0)))
+  {
+  }
+  return URDO
+void UARTwrite(char data)
+{
+    while(!(UCSR0A & (1<<UDRE0)))
+    {
+
+    }
+    UDR0 = data;
+}
