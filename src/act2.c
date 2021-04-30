bfe6bca5d7878d093667e69aac2e92c84c57f58b
206819 <81298446+206819@users.noreply.github.com>
2021-04-30 18:07:01 +0530

    Update act2.c

--- src/act2.c
+++ src/act2.c
@@ -8,7 +8,17 @@
  * @copyright Copyright (c) 2021
  * 
  */
+#include <avr/io.h>
 
+void act2();
+{
+    DDRB|=(1<<PB0);//Set B0=1 for led
+    DDRD&=~(1<<PD0);// clear bit
+    PORTD|=(1<<PD0);//set bit
+    DDRD&=~(1<<PD1);//clear bit
+    PORTD|=(1<<PD1);//set bit
+
+}
 
 void initADC()
    {
