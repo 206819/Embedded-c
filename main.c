6430556bf32af3d384071e34d6b3b370e7440a02
206819 <81298446+206819@users.noreply.github.com>
2021-04-30 20:39:34 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,30 @@
+/**
+ * @file main.c
+ * @author 206819(you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "act1.h"
+#include "act2.h"
+#include "act3.h"
+#include<avr/io.h>
+#include<util/delay.h>
 
+uint16_t temp;
+initADC();
+char temp;
+uint16_t temp;
+
+int main(void)
+{
+  while(1)
+  {
+    temp=ReadADC(0);
+    _delay_ms(10);
+  }
+  return 0;
+}
