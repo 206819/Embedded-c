d5ca49f07453e1c4093e20d6168c301e98bbcbc0
206819 <81298446+206819@users.noreply.github.com>
2021-04-30 18:34:40 +0530

    Update act1.c

--- src/act1.c
+++ src/act1.c
@@ -9,13 +9,33 @@
  * 
  */
 #include <avr/io.h>
-
-void act1();
+#include<util/delay.h>
+int main(void)
 {
     DDRB|=(1<<PB0);//Set B0=1 for led
     DDRD&=~(1<<PD0);// clear bit
     PORTD|=(1<<PD0);//set bit
-    DDRD&=~(1<<PD1);//clear bit
+    DDRD&=~(1<<PD1);//clear bit 
     PORTD|=(1<<PD1);//set bit
 
+    while(1)
+    {
+       if(!(PIND&(1<<PD0)))
+        {
+            if(!(PIND&(1<<PD1)))
+            {
+                 PORTB|=(1<<PB0);//passanger and heater button indication
+                 _delay_ms(500);
+            }
+
+
+              else
+            {
+                PORTB&=~(1<<PB0);
+                _delay_ms(500);
+            }
+
+        }
+    }
+      return 0;
 }
