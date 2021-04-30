27257d2e77205637f867c10814fbcfc463f4e288
206819 <81298446+206819@users.noreply.github.com>
2021-04-30 18:07:52 +0530

    Update act3.c

--- src/act3.c
+++ src/act3.c
@@ -1,2 +1,12 @@
+/**
+ * @file act3.c
+ * @author 206819
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <avr/io.h>
 #include<util/delay.h>
