9ffff47eca1647b0be6191d2a03037e2c1aaf83b
206819 <81298446+206819@users.noreply.github.com>
2021-04-30 18:06:03 +0530

    Update act2.c

--- src/act2.c
+++ src/act2.c
@@ -1,32 +1,14 @@
-#include <avr/io.h>
-#include<util/delay.h>
-int main(void)
-{
-    DDRB|=(1<<PB0);//Set B0=1 for led
-    DDRD&=~(1<<PD0);// clear bit
-    PORTD|=(1<<PD0);//set bit
-    DDRD&=~(1<<PD1);//clear bit
-    PORTD|=(1<<PD1);//set bit
+/**
+ * @file act2.c
+ * @author 206819
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
-    while(1)
-    {
-       if(!(PIND&(1<<PD0)))
-        {
-            if(!(PIND&(1<<PD1)))
-            {
-                 PORTB|=(1<<PB0);//passanger and heater button indication
-                 _delay_ms(200);
-            }
-
-
-              else
-            {
-                PORTB&=~(1<<PB0);
-                _delay_ms(200);
-            }
-
-        }
-    }
 
 void initADC()
    {
