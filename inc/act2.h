6a66bf6b66f2540197e1722f6d5cbba2c5829102
206819 <81298446+206819@users.noreply.github.com>
2021-04-30 12:15:47 +0530

    Update act2.h

--- inc/act2.h
+++ inc/act2.h
@@ -2,6 +2,13 @@
 #define ACT2_H_INCLUDED
 
 void initADC();
+/** \brief Reads the temperature to a channel on MCU
+ *
+ * \param ch is the channel number in which the conversion takes place
+ * \param
+ * \return It returns a 16 bit number
+ *
+ */
 
 uint16_t Read_TEMP(uint8_t ch);
 
